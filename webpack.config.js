47d9f29894b5dfaa451c267a2feaf70e0d9bde13
David Bentley <david.bentley95@gmail.com>
2025-03-28 16:20:01 -0700

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./index.html"],
+    compress: true,
+    port: 8080,
+    open: true, // Automatically open the browser
+    hot: true, // Enable Hot Module Replacement (HMR)
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      }
+    ],
+  },
+};
